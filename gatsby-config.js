789706451e1bedb38ba25e85a6497edbe7ccd8d8
Adrian Campos <adriancampos@users.noreply.github.com>
2020-04-12 12:57:14 -0700

    Add pathPrefix to gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,7 @@ module.exports = {
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@adriancampos`,
   },
+  pathPrefix: "/pitch-pipe",
   plugins: [
     `gatsby-plugin-react-helmet`,
     {
